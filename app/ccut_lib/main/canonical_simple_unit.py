1b8eb7db41fdba74c5f5bc3dddbca70c64a97029
Basel Shbita <basel921@gmail.com>
2019-07-02 00:36:19 -0700

    fixes #8

--- app/ccut_lib/main/canonical_simple_unit.py
+++ app/ccut_lib/main/canonical_simple_unit.py
@@ -10,9 +10,14 @@ from .symbol_map import SymbolMap, INDEX_NAME_URI, INDEX_NAME_PREFIX, INDEX_NAME
 from .unit import Unit
 from .unit_match import UnitMatch
 
-QUDT_V1_FIXED_ONTOLOGY_PREF = "http://www.qudt.org/qudt/owl/1.0.0/unit/Instances.html#"
+QUDT_V1_NAMESPACE_INVALID = 'http://data.nasa.gov/qudt/owl/unit#'
+QUDT_V1_NAMESPACE_VALID = 'http://www.qudt.org/qudt/owl/1.0.0/unit/Instances.html#'
 QUDT_PROPERTIES_NAMESPACE = 'qudtp'
 CCUT_NAMESPACE = 'ccut'
+
+def fix_qudt_namespace_if_exists(original_uri):
+    new_uri = original_uri.replace(QUDT_V1_NAMESPACE_INVALID, QUDT_V1_NAMESPACE_VALID)
+    return new_uri
 
 class CanonicalSimpleUnit:
     def __init__(self, unit: Unit):
@@ -38,9 +43,9 @@ class CanonicalSimpleUnit:
 
     def set_quantity(self, qudt_unit):
         if qudt_unit is not None:
-            self.unit_obj[f'{QUDT_PROPERTIES_NAMESPACE}:quantityKind'] =  QUDT_V1_FIXED_ONTOLOGY_PREF + qudt_unit[INDEX_NAME_URI].split('#')[1]
+            self.unit_obj[f'{QUDT_PROPERTIES_NAMESPACE}:quantityKind'] = fix_qudt_namespace_if_exists(qudt_unit[INDEX_NAME_URI])
             if qudt_unit[INDEX_NAME_PREFIX] is not None:
-                self.unit_obj[f'{CCUT_NAMESPACE}:prefix'] = QUDT_V1_FIXED_ONTOLOGY_PREF + qudt_unit[INDEX_NAME_PREFIX].split('#')[1]
+                self.unit_obj[f'{CCUT_NAMESPACE}:prefix'] = fix_qudt_namespace_if_exists(qudt_unit[INDEX_NAME_PREFIX])
                 self.unit_obj[f'{CCUT_NAMESPACE}:prefixConversionMultiplier'] = qudt_unit[INDEX_NAME_PREFIX_CONVERSION_MULTIPLIER]
                 self.unit_obj[f'{CCUT_NAMESPACE}:prefixConversionOffset'] = qudt_unit[INDEX_NAME_PREFIX_CONVERSION_OFFSET]
         else:
